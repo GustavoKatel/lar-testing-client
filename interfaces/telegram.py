957b82e18f912d827eaa43249e0c0acb53f0830f
Gustavo Sampaio <gbritosampaio@gmail.com>
2016-07-07 00:24:26 -0300

    logs, dump, download, upload

--- interfaces/telegram.py
+++ interfaces/telegram.py
@@ -2,6 +2,7 @@ import telepot
 import time
 import threading
 import re
+import os
 
 from node import Status as NodeStatus
 from node import Node
@@ -19,11 +20,32 @@ class Telegram(threading.Thread):
         content_type, chat_type, chat_id = telepot.glance(msg)
         print(content_type, chat_type, chat_id)
 
-        if content_type == 'text':
+        # print msg
+        if not msg['from']['username'] in self.conf['telegram']['admins']:
+            print "Not authorized: %s" % msg['from']
+            self.bot.sendMessage(chat_id, """
+-.-.-00-.-.-
+>>> OCToPUS <<<<
+-.-.-00-.-.-
+            """)
+            return
+
+        if content_type == 'document':
+            doc = msg['document']
+            cdir = os.path.dirname(os.path.realpath(__file__))
+            print "Downloading... " + doc['file_name']
+            fd = open(cdir+'/../data/'+doc['file_name'], 'w')
+            self.bot.download_file(doc['file_id'], fd)
+            fd.close()
+
+            for node in self.nodes:
+                node.putFile(cdir+'/../data/'+doc['file_name'], doc['file_name'])
+
+        elif content_type == 'text':
             text = msg['text']
 
-            # /list
-            if re.match(r'/list.*', text):
+            # /info
+            if re.match(r'/info$', text):
                 res = "Nodes list starts -.-.-00-.-.-\n\n"
                 for i,node in enumerate(self.nodes):
                     res += str(i) + "- " + str(node) + "\n"
@@ -47,8 +69,8 @@ class Telegram(threading.Thread):
                         self.bot.sendMessage(chat_id, res)
 
             # /exec [LIST_OF_NAMES|*] CMD
-            elif re.match(r'/exec \[([a-zA-Z0-9_,]+|\*)\] (.+)', text):
-                m = re.match(r'/exec \[([a-zA-Z0-9_,]+|\*)\] (.+)', text)
+            elif re.match(r'/exec ([a-zA-Z0-9_,]+|\*) (.+)', text):
+                m = re.match(r'/exec ([a-zA-Z0-9_,]+|\*) (.+)', text)
                 names = m.group(1)
                 cmd = m.group(2)
 
@@ -65,8 +87,8 @@ class Telegram(threading.Thread):
                     node.runCommand(cmd)
 
             # /connect [LIST_OF_NAMES|*]
-            elif re.match(r'/connect \[([a-zA-Z0-9_,]+|\*)\] (.+)', text):
-                m = re.match(r'/connect \[([a-zA-Z0-9_,]+|\*)\] (.+)', text)
+            elif re.match(r'/connect ([a-zA-Z0-9_,]+|\*)', text):
+                m = re.match(r'/connect ([a-zA-Z0-9_,]+|\*)', text)
                 names = m.group(1)
 
                 current_nodes = []
@@ -79,7 +101,77 @@ class Telegram(threading.Thread):
                             current_nodes.append(node)
 
                 for node in current_nodes:
+                    self.bot.sendMessage(chat_id, "Connecting to: %s -.-.-00-.-.-\n\n" % name)
                     node.connect()
+
+            # /logs [LIST_OF_NAMES|*]
+            elif re.match(r'/logs ([a-zA-Z0-9_,]+|\*)', text):
+                m = re.match(r'/logs ([a-zA-Z0-9_,]+|\*)', text)
+                names = m.group(1)
+
+                current_nodes = []
+                if names == "*":
+                    current_nodes = self.nodes
+                else:
+                    lnames = names.split(',')
+                    for node in self.nodes:
+                        if node.name in lnames:
+                            current_nodes.append(node)
+
+                for node in current_nodes:
+                    cdir = os.path.dirname(os.path.realpath(__file__))
+                    f = open(cdir+"/../nodes/"+node.name+".out", "r")
+
+                    res = "Reading logs: %s -.-.-00-.-.-\n\n" % node.name
+                    res += ''.join(f.readlines())
+                    res += "Reading logs ends: %s -.-.-00-.-.-\n\n" % node.name
+                    self.bot.sendMessage(chat_id, res)
+
+                    f.close()
+
+            # /dump [LIST_OF_NAMES|*] [stdout|stderr]
+            elif re.match(r'/dump ([a-zA-Z0-9_,]+|\*) (stdout|stderr)', text):
+                m = re.match(r'/dump ([a-zA-Z0-9_,]+|\*) (stdout|stderr)', text)
+                names = m.group(1)
+                dump_type = m.group(2)
+
+                current_nodes = []
+                if names == "*":
+                    current_nodes = self.nodes
+                else:
+                    lnames = names.split(',')
+                    for node in self.nodes:
+                        if node.name in lnames:
+                            current_nodes.append(node)
+
+                for node in current_nodes:
+                    cdir = os.path.dirname(os.path.realpath(__file__))
+                    if dump_type == "stdout":
+                        f = open(cdir+"/../nodes/"+node.name+".out", "r")
+                    else:
+                        f = open(cdir+"/../nodes/"+node.name+".err", "r")
+
+                    self.bot.sendDocument(chat_id, f, "%s - %s" % (node.name, dump_type))
+
+            # /download [LIST_OF_NAMES|*] filename
+            elif re.match(r'/download ([a-zA-Z0-9_,]+|\*) (.*)', text):
+                m = re.match(r'/download ([a-zA-Z0-9_,]+|\*) (.*)', text)
+                names = m.group(1)
+                filename = m.group(2)
+
+                current_nodes = []
+                if names == "*":
+                    current_nodes = self.nodes
+                else:
+                    lnames = names.split(',')
+                    for node in self.nodes:
+                        if node.name in lnames:
+                            current_nodes.append(node)
+
+                for node in current_nodes:
+                    f = node.getFile(filename)
+                    self.bot.sendDocument(chat_id, f, "%s - %s" % (node.name, filename))
+
 
             else:
                 self.bot.sendMessage(chat_id, self.helpText())
@@ -105,7 +197,11 @@ class Telegram(threading.Thread):
 -.-.-00-.-.-
 
 Commands:
-/list: Returns the list of nodes in the server
+/info: Returns the list of nodes in the server
 /info NAME: Returns information about a specif node
-/exec [LIST_OF_NAMES|*] CMD: Execute the command CMD. Eg.: /exec [s1,s2] ls -lah
+/exec LIST_OF_NAMES|* CMD: Execute the command CMD. Eg.: /exec [s1,s2] ls -lah
+/connect LIST_OF_NAMES|*: Force a connection to the nodes in LIST_OF_NAMES
+/logs LIST_OF_NAMES|*: Read the stdout from the node
+/dump LIST_OF_NAMES|* stdout|stderr: Download the stdout or the stderr from a node
+/download LIST_OF_NAMES|* FILENAME: Download FILENAME from a node
 """
--- node.py
+++ node.py
@@ -40,12 +40,28 @@ class Node:
         self.currentCommand = ""
         self.lastCommand = ""
 
+        cdir = os.path.dirname(os.path.realpath(__file__))
+        if not os.path.exists(cdir+"/nodes"):
+            os.makedirs(cdir+"/nodes")
+
+        self.fstdout = open(cdir+"/nodes/"+self.name+".out", "w")
+        self.fstderr = open(cdir+"/nodes/"+self.name+".err", "w")
+        self.fnodeerr = open(cdir+"/nodes/"+self.name+".nodeerr", "w")
+
+        self.currentStdin = None
+
     def close(self):
         print "closing connection"
         self.ssh.close()
+        self.status = Status.closed
         print "closed"
 
+    # def kill(self):
+        # if self.status = Status.running and self.currentStdin:
+
+
     def runCommand(self, cmd):
+        # Only one command at a time
         if self.status == Status.running:
             return False
 
@@ -62,14 +78,28 @@ class Node:
 
             stdin, stdout, stderr = self.ssh.exec_command(cmd)
 
+            self.currentStdin = stdin
+
+            print "done..."
+
             self.lastCommand = cmd
             self.currentCommand = ""
 
             self.status = Status.idle
+            self.fstdout.write('-.-.-00-.-.- %s -.-.-00-.-.-\n' % cmd)
             for line in stdout:
-                print '... ' + line.strip('\n')
+                self.fstdout.write(line)
+            self.fstdout.write('-.-.-00-.-.--.-.-00-.-.--.-.-00-.-.-\n')
+            self.fstdout.flush()
+
+            self.fstderr.write('-.-.-00-.-.- %s -.-.-00-.-.-\n' % cmd)
+            for line in stderr:
+                self.fstderr.write(line)
+            self.fstderr.write('-.-.-00-.-.--.-.-00-.-.--.-.-00-.-.-\n')
+            self.fstderr.flush()
+
         except:
-            return
+            self.close()
 
     def connect(self):
         self.ssh = paramiko.SSHClient()
@@ -83,11 +113,26 @@ class Node:
                 key_filename=self.pkey,
                 port=int(self.port)
             )
+
             self.status = Status.idle
             print "connected"
         except Exception as e:
             print e
             self.status = Status.closed
+
+    def getFile(self, filename):
+        if self.status == Status.closed or self.status == Status.connecting:
+            return
+
+        sftp = self.ssh.open_sftp()
+        return sftp.open(filename, 'r')
+
+    def putFile(self, filename, dest_name):
+        if self.status == Status.closed or self.status == Status.connecting:
+            return
+
+        sftp = self.ssh.open_sftp()
+        sftp.put(filename, dest_name)
 
     def __str__(self):
         return self.__unicode__()
