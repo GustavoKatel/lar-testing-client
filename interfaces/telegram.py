dbd8c4d8681e4c7b13564e5981f09155cb43c51c
Gustavo Sampaio <gbritosampaio@gmail.com>
2016-07-08 17:31:02 -0300

    Telegram: inline buttons

--- interfaces/telegram.py
+++ interfaces/telegram.py
@@ -1,9 +1,12 @@
-import telepot
 import time
 import threading
 import re
 import os
 import json
+
+import telepot
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardHide, ForceReply
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
 from node import Status as NodeStatus
 from node import Node
@@ -17,17 +20,20 @@ class Telegram(threading.Thread):
         self.nodes = nodes
         self.conf = conf
 
-    def filter_nodes(self, names):
-        current_nodes = []
-        if names == "*":
-            current_nodes = self.nodes
-        else:
-            lnames = names.split(',')
-            for node in self.nodes:
-                if node.name in lnames:
-                    current_nodes.append(node)
+    def handle_inline(self, msg):
+        query_id, from_id, data = telepot.glance(msg, flavor='callback_query')
+        print('Callback query:', query_id, from_id, data)
 
-        return current_nodes
+        if not msg['from']['username'] in self.conf['telegram']['admins']:
+            print "Not authorized: %s" % msg['from']
+            self.bot.sendMessage(chat_id, """
+-.-.-00-.-.-
+>>> OCToPUS <<<<
+-.-.-00-.-.-
+            """)
+            return
+
+        self.interpreter(from_id, data)
 
     def handle(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
@@ -57,147 +63,250 @@ class Telegram(threading.Thread):
         elif content_type == 'text':
             text = msg['text']
 
-            # /info
-            if re.match(r'/info$', text):
-                self.bot.sendChatAction(chat_id, "typing")
-                res = "Nodes list starts -.-.-00-.-.-\n\n"
-                for i,node in enumerate(self.nodes):
+            self.interpreter(chat_id, text)
+
+    def filter_nodes(self, names):
+        current_nodes = []
+        if names == "*":
+            current_nodes = self.nodes
+        else:
+            lnames = names.split(',')
+            for node in self.nodes:
+                if node.name in lnames:
+                    current_nodes.append(node)
+
+        return current_nodes
+
+    def interpreter(self, chat_id, text):
+        # /start
+        if re.match(r'/start$', text):
+            markup = InlineKeyboardMarkup(inline_keyboard=[
+                     [dict(text='System information', callback_data='/info'),
+                     dict(text='Node information', callback_data='/infoNode')],
+                     [dict(text='Execute command', callback_data='/executeNode'),
+                     dict(text='Read logs', callback_data='/logsNode')],
+                     [dict(text='Download file', callback_data='/downloadNode'),
+                     dict(text='Kill process', callback_data='/killNode')],
+                 ])
+
+            self.bot.sendMessage(chat_id, '>>> OCToPUS <<<<', reply_markup=markup)
+
+        # /infoNode inline keyboard
+        elif re.match(r'/infoNode$', text):
+            markup = InlineKeyboardMarkup(inline_keyboard=[
+                     [dict(text=node.name, callback_data='/info %s' % node.name)] for node in self.nodes
+                 ])
+
+            self.bot.sendMessage(chat_id, '>>> OCToPUS <<<<\nChoose a node', reply_markup=markup)
+
+        # /executeNode inline keyboard
+        elif re.match(r'/executeNode$', text):
+
+
+            self.bot.sendMessage(chat_id, """
+            >>> OCToPUS <<<<\nPlease send:\n /exec LIST_OF_NAMES|* CMD: Execute the command CMD. Eg.: /exec s1,s2 ls -lah
+            """
+            )
+
+        # /logsNode inline keyboard
+        elif re.match(r'/logsNode$', text):
+            buttons = []
+            buttons += [dict(text=node.name, callback_data='/logs %s' % node.name) for node in self.nodes]
+            markup = InlineKeyboardMarkup(inline_keyboard=[
+                    [dict(text="All nodes", callback_data='/logs *')],
+                    buttons
+                 ])
+
+            self.bot.sendMessage(chat_id, '>>> OCToPUS <<<<\nChoose a node', reply_markup=markup)
+
+        # /downloadNode inline keyboard
+        elif re.match(r'/downloadNode$', text):
+
+            self.bot.sendMessage(chat_id, """
+            >>> OCToPUS <<<<\nPlease send:\n /download LIST_OF_NAMES|* FILENAME: Download FILENAME from a node
+            """
+            )
+
+        # /killNode inline keyboard
+        elif re.match(r'/killNode$', text):
+            buttons = []
+            buttons += [dict(text=node.name, callback_data='/killNode %s' % node.name) for node in self.nodes]
+            markup = InlineKeyboardMarkup(inline_keyboard=[
+                    # [dict(text="All nodes", callback_data='/killNode *')],
+                    buttons
+                 ])
+
+            self.bot.sendMessage(chat_id, '>>> OCToPUS <<<<\nChoose a node', reply_markup=markup)
+
+        # /killNode inline keyboard - choose process
+        elif re.match(r'/killNode ([a-zA-Z0-9_,]+|\*)', text):
+            m = re.match(r'/killNode ([a-zA-Z0-9_,]+|\*)', text)
+            names = m.group(1)
+
+            current_nodes = self.filter_nodes(names)
+            self.bot.sendChatAction(chat_id, "typing")
+            for node in current_nodes:
+                node.updateProcessList()
+
+                if len(node.processList)<=0:
+                    self.bot.sendMessage(chat_id, 'No process found. See /help')
+                    continue
+
+                markup = InlineKeyboardMarkup(inline_keyboard=[
+                        [dict(text=process, callback_data='/exec %s kill -9 %s' % ( node.name, process.split(' ')[0] )) for process in node.processList]
+                     ])
+
+                self.bot.sendMessage(chat_id, 'Choose a process', reply_markup=markup)
+
+        # /info
+        elif re.match(r'/info$', text):
+            self.bot.sendChatAction(chat_id, "typing")
+            res = "Nodes list starts -.-.-00-.-.-\n\n"
+            for i,node in enumerate(self.nodes):
+                node.updateProcessList()
+                res += str(i) + "- " + str(node) + "\n"
+            res += "\nNodes list ends -.-.-00-.-.-\n"
+            self.bot.sendMessage(chat_id, res)
+
+        # /info NAME
+        elif re.match(r'/info [a-zA-Z0-9_]+', text):
+            m = re.match(r'/info ([a-zA-Z0-9_]+)', text)
+            name = m.group(1)
+            for node in self.nodes:
+                if node.name == name:
+                    self.bot.sendChatAction(chat_id, "typing")
                     node.updateProcessList()
-                    res += str(i) + "- " + str(node) + "\n"
-                res += "\nNodes list ends -.-.-00-.-.-\n"
-                self.bot.sendMessage(chat_id, res)
-
-            # /info NAME
-            elif re.match(r'/info [a-zA-Z0-9_]+', text):
-                m = re.match(r'/info ([a-zA-Z0-9_]+)', text)
-                name = m.group(1)
-                for node in self.nodes:
-                    if node.name == name:
-                        self.bot.sendChatAction(chat_id, "typing")
-                        node.updateProcessList()
-                        res = "Node info: %s -.-.-00-.-.-\n\n" % name
-                        res += "hostname: %s\n" % node.hostname
-                        res += "username: %s\n" % node.username
-                        res += "port: %s\n" % node.port
-                        res += "status: %s\n" % NodeStatus.toStr(node.status)
-                        res += "last command: %s\n" % node.lastCommand
-                        res += "Process count: %s\n" % node.processCount
-                        res += "Process List:\n"
-                        for p in node.processList:
-                            res += "%s\n" % p
-                        res += "\n"
-                        res += "\nNode info ends -.-.-00-.-.-\n"
-                        self.bot.sendMessage(chat_id, res)
-
-            # /exec [LIST_OF_NAMES|*] CMD
-            elif re.match(r'/exec ([a-zA-Z0-9_,]+|\*) (.+)', text):
-                m = re.match(r'/exec ([a-zA-Z0-9_,]+|\*) (.+)', text)
-                names = m.group(1)
-                cmd = m.group(2)
-
-                current_nodes = self.filter_nodes(names)
-                for node in current_nodes:
-                    node.runCommand(cmd)
-
-            # /connect [LIST_OF_NAMES|*]
-            elif re.match(r'/connect ([a-zA-Z0-9_,]+|\*)', text):
-                m = re.match(r'/connect ([a-zA-Z0-9_,]+|\*)', text)
-                names = m.group(1)
-
-                current_nodes = self.filter_nodes(names)
-                for node in current_nodes:
-                    self.bot.sendChatAction(chat_id, "typing")
-                    self.bot.sendMessage(chat_id, "Connecting to: %s -.-.-00-.-.-\n\n" % name)
-                    node.connect()
-
-            # /logs [LIST_OF_NAMES|*]
-            elif re.match(r'/logs ([a-zA-Z0-9_,]+|\*)', text):
-                m = re.match(r'/logs ([a-zA-Z0-9_,]+|\*)', text)
-                names = m.group(1)
-
-                current_nodes = self.filter_nodes(names)
-                for node in current_nodes:
-                    cdir = os.path.dirname(os.path.realpath(__file__))
-                    f = open(cdir+"/../nodes/"+node.name+".out", "r")
-
-                    self.bot.sendChatAction(chat_id, "typing")
-                    res = "Reading logs: %s -.-.-00-.-.-\n\n" % node.name
-                    res += ''.join(f.readlines())
-                    res += "Reading logs ends: %s -.-.-00-.-.-\n\n" % node.name
+                    res = "Node info: %s -.-.-00-.-.-\n\n" % name
+                    res += "hostname: %s\n" % node.hostname
+                    res += "username: %s\n" % node.username
+                    res += "port: %s\n" % node.port
+                    res += "status: %s\n" % NodeStatus.toStr(node.status)
+                    res += "last command: %s\n" % node.lastCommand
+                    res += "Process count: %s\n" % node.processCount
+                    res += "Process List:\n"
+                    for p in node.processList:
+                        res += "%s\n" % p
+                    res += "\n"
+                    res += "\nNode info ends -.-.-00-.-.-\n"
                     self.bot.sendMessage(chat_id, res)
 
-                    f.close()
+        # /exec [LIST_OF_NAMES|*] CMD
+        elif re.match(r'/exec ([a-zA-Z0-9_,]+|\*) (.+)', text):
+            m = re.match(r'/exec ([a-zA-Z0-9_,]+|\*) (.+)', text)
+            names = m.group(1)
+            cmd = m.group(2)
 
-            # /dump [LIST_OF_NAMES|*] [stdout|stderr]
-            elif re.match(r'/dump ([a-zA-Z0-9_,]+|\*) (stdout|stderr)', text):
-                m = re.match(r'/dump ([a-zA-Z0-9_,]+|\*) (stdout|stderr)', text)
-                names = m.group(1)
-                dump_type = m.group(2)
+            current_nodes = self.filter_nodes(names)
+            for node in current_nodes:
+                node.runCommand(cmd)
 
-                current_nodes = self.filter_nodes(names)
-                for node in current_nodes:
-                    cdir = os.path.dirname(os.path.realpath(__file__))
-                    if dump_type == "stdout":
-                        f = open(cdir+"/../nodes/"+node.name+".out", "r")
-                    else:
-                        f = open(cdir+"/../nodes/"+node.name+".err", "r")
+        # /connect [LIST_OF_NAMES|*]
+        elif re.match(r'/connect ([a-zA-Z0-9_,]+|\*)', text):
+            m = re.match(r'/connect ([a-zA-Z0-9_,]+|\*)', text)
+            names = m.group(1)
 
-                    self.bot.sendChatAction(chat_id, "upload_document")
-                    self.bot.sendDocument(chat_id, f, "%s - %s" % (node.name, dump_type))
+            current_nodes = self.filter_nodes(names)
+            for node in current_nodes:
+                self.bot.sendChatAction(chat_id, "typing")
+                self.bot.sendMessage(chat_id, "Connecting to: %s -.-.-00-.-.-\n\n" % name)
+                node.connect()
 
-            # /download [LIST_OF_NAMES|*] filename
-            elif re.match(r'/download ([a-zA-Z0-9_,]+|\*) (.+)', text):
-                m = re.match(r'/download ([a-zA-Z0-9_,]+|\*) (.+)', text)
-                names = m.group(1)
-                filename = m.group(2)
+        # /logs [LIST_OF_NAMES|*]
+        elif re.match(r'/logs ([a-zA-Z0-9_,]+|\*)', text):
+            m = re.match(r'/logs ([a-zA-Z0-9_,]+|\*)', text)
+            names = m.group(1)
 
-                current_nodes = self.filter_nodes(names)
-                for node in current_nodes:
-                    f = node.getFile(filename)
-                    self.bot.sendChatAction(chat_id, "upload_document")
-                    self.bot.sendDocument(chat_id, f, "%s - %s" % (node.name, filename))
-
-            # /auth USERNAME
-            elif re.match(r'/auth ([a-zA-Z0-9_]+)', text):
-                m = re.match(r'/auth ([a-zA-Z0-9_]+)', text)
-                username = m.group(1)
-                self.conf['telegram']['admins'].append(username)
-
-                js = json.dumps(self.conf, ensure_ascii=False, sort_keys=True, indent=4)
-                with open('octopus.conf', 'w') as f:
-                    f.write(js+'\n')
+            current_nodes = self.filter_nodes(names)
+            for node in current_nodes:
+                cdir = os.path.dirname(os.path.realpath(__file__))
+                f = open(cdir+"/../nodes/"+node.name+".out", "r")
 
                 self.bot.sendChatAction(chat_id, "typing")
-                self.bot.sendMessage(chat_id, "New admin added: %s -.-.-00-.-.-\n\n" % username)
+                res = "Reading logs: %s -.-.-00-.-.-\n\n" % node.name
+                res += ''.join(f.readlines())
+                res += "Reading logs ends: %s -.-.-00-.-.-\n\n" % node.name
+                self.bot.sendMessage(chat_id, res)
 
-            # /killall [LIST_OF_NAMES|*]
-            elif re.match(r'/killall ([a-zA-Z0-9_,]+|\*)', text):
-                m = re.match(r'/killall ([a-zA-Z0-9_,]+|\*)', text)
-                names = m.group(1)
+                f.close()
 
-                current_nodes = self.filter_nodes(names)
-                for node in current_nodes:
-                    node.runCommand("pkill -f \"OCTOPUS\"")
+        # /dump [LIST_OF_NAMES|*] [stdout|stderr]
+        elif re.match(r'/dump ([a-zA-Z0-9_,]+|\*) (stdout|stderr)', text):
+            m = re.match(r'/dump ([a-zA-Z0-9_,]+|\*) (stdout|stderr)', text)
+            names = m.group(1)
+            dump_type = m.group(2)
 
-            # /killall [LIST_OF_NAMES|*] CMD_PATTERN
-            elif re.match(r'/killall ([a-zA-Z0-9_,]+|\*) (.+)', text):
-                m = re.match(r'/killall ([a-zA-Z0-9_,]+|\*) (.+)', text)
-                names = m.group(1)
-                pattern = m.group(2)
+            current_nodes = self.filter_nodes(names)
+            for node in current_nodes:
+                cdir = os.path.dirname(os.path.realpath(__file__))
+                if dump_type == "stdout":
+                    f = open(cdir+"/../nodes/"+node.name+".out", "r")
+                else:
+                    f = open(cdir+"/../nodes/"+node.name+".err", "r")
 
-                current_nodes = self.filter_nodes(names)
-                for node in current_nodes:
-                    node.runCommand("pkill -f %s" % pattern)
+                self.bot.sendChatAction(chat_id, "upload_document")
+                self.bot.sendDocument(chat_id, f, "%s - %s" % (node.name, dump_type))
 
-            else:
-                self.bot.sendMessage(chat_id, self.helpText())
+        # /download [LIST_OF_NAMES|*] filename
+        elif re.match(r'/download ([a-zA-Z0-9_,]+|\*) (.+)', text):
+            m = re.match(r'/download ([a-zA-Z0-9_,]+|\*) (.+)', text)
+            names = m.group(1)
+            filename = m.group(2)
+
+            current_nodes = self.filter_nodes(names)
+            for node in current_nodes:
+                f = node.getFile(filename)
+                self.bot.sendChatAction(chat_id, "upload_document")
+                self.bot.sendDocument(chat_id, f, "%s - %s" % (node.name, filename))
+
+        # /auth USERNAME
+        elif re.match(r'/auth ([a-zA-Z0-9_]+)', text):
+            m = re.match(r'/auth ([a-zA-Z0-9_]+)', text)
+            username = m.group(1)
+            self.conf['telegram']['admins'].append(username)
+
+            js = json.dumps(self.conf, ensure_ascii=False, sort_keys=True, indent=4)
+            with open('octopus.conf', 'w') as f:
+                f.write(js+'\n')
+
+            self.bot.sendChatAction(chat_id, "typing")
+            self.bot.sendMessage(chat_id, "New admin added: %s -.-.-00-.-.-\n\n" % username)
+
+        # /killall [LIST_OF_NAMES|*]
+        elif re.match(r'/killall ([a-zA-Z0-9_,]+|\*)', text):
+            m = re.match(r'/killall ([a-zA-Z0-9_,]+|\*)', text)
+            names = m.group(1)
+
+            current_nodes = self.filter_nodes(names)
+            for node in current_nodes:
+                node.runCommand("pkill -f \"OCTOPUS\"")
+
+        # /killall [LIST_OF_NAMES|*] CMD_PATTERN
+        elif re.match(r'/killall ([a-zA-Z0-9_,]+|\*) (.+)', text):
+            m = re.match(r'/killall ([a-zA-Z0-9_,]+|\*) (.+)', text)
+            names = m.group(1)
+            pattern = m.group(2)
+
+            current_nodes = self.filter_nodes(names)
+            for node in current_nodes:
+                node.runCommand("pkill -f %s" % pattern)
+
+        # /help
+        elif re.match(r'/help$', text):
+            self.bot.sendMessage(chat_id, self.helpText())
+
+        else:
+            self.interpreter(chat_id, "/start")
 
     def stop(self):
         self.running = False
 
     def run(self):
         self.bot = telepot.Bot(self.conf["telegram"]["token"])
-        self.bot.message_loop(self.handle)
+        self.bot.message_loop({
+            'chat': self.handle,
+            'callback_query': self.handle_inline,
+            'inline_query': self.handle_inline
+        })
         self.running = True
 
         while self.running:
